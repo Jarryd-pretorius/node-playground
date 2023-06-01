6365c09b611dcfbf3395fb14fdfec713b5a74437
Jarryd-pretorius <jarrydpretorius6@gmail.com>
2023-06-01 09:19:50 +0800

    changed ports around

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { readFile } = require("fs").promises;
-
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.get("/", async (request, response) => {
   response.send(await readFile("./home.html", "utf8"));
 });
 
-app.listen(3000, () => {
-  console.log("app running on localHost: 3000");
+app.listen(port, () => {
+  console.log("server Started");
 });
